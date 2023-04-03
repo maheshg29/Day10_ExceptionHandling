4ea0200e5cf6d24e3062dde7c3a9bfecc761993a
mahesh <gangurdemahesh9@gmail.com>
2023-04-03 19:44:52 +0530

    [mahesh]add program file for categorized base on age

--- Day10-ExceptionHandling/Program.cs
+++ Day10-ExceptionHandling/Program.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Day10_ExceptionHandling
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Day 10 Exception Handling");
+            Console.WriteLine("Enter your Age");
+            int age=Convert.ToInt32(Console.ReadLine());
+            Categorized categorized = new Categorized();
+            categorized.GetCategory(age);
         }
     }
 }
