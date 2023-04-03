817e181271f13c00410ab6796c188504a5b2f782
mahesh <gangurdemahesh9@gmail.com>
2023-04-03 19:35:21 +0530

    [mahesh]add welcome greeting message

---
+++ Day10-ExceptionHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 10 Exception Handling");
+        }
+    }
+}
